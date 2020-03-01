2b963649c1ed65a247151b69f0841d2d52298a67
landonbuell <lhb1007@wildcats.unh.edu>
2020-03-01 18:04:24 -0500

    1 March 2020
    - begun Program to impliment MLP Classifier on MNIST dataset
    - Paper to breakdown source code for MLPClassifier object
    - Saved example confusion matrix for refrence: layers = (20,20,20,20)

---
+++ Notes/Multi_Layer_Perceptron/MLPClassifier_v0.py
@@ -0,0 +1,51 @@
+"""
+Landon Buell
+Prof. Yu
+MLP Classifier
+1 March 2020
+"""
+
+            #### IMPORTS ####
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import fetch_openml
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+            #### MAIN EXECUTABLE ####
+
+if __name__ == '__main__':
+
+    # load in data set - MNIST
+    MNIST = fetch_openml(name='mnist_784',version=1)
+    X,y = MNIST['data'],MNIST['target']
+
+    # create instance of MLP Classifier object
+    layers = (20,20,20,20)
+    MLP_CLF = MLPClassifier(hidden_layer_sizes=layers,activation='relu',
+                            solver='sgd',max_iter=200,tol=1e-4,
+                            random_state=0)
+
+    # split the data into training a testing sets
+    Xtrain,Xtest,ytrain,ytest = \
+        train_test_split(X,y,test_size=0.4,random_state=42)
+
+    # Train the Classifier Instance
+    MLP_CLF.fit(Xtrain,ytrain)
+
+    # Test & Evaluate Classifer
+    ypred = MLP_CLF.predict(Xtest)
+
+    confmat = confusion_matrix(ytest,ypred)
+    plt.imshow(confmat,cmap=plt.cm.binary)
+    plt.title("MNIST Confusion Matrix",size=20,weight='bold')
+    plt.xticks(np.arange(0,10,1))
+    plt.yticks(np.arange(0,10,1))
+    plt.show()
+
+
+
+
