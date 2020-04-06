83587afc2d858472c9af62caa1a966a6d2f65813
landonbuell <lhb1007@wildcats.unh.edu>
2020-04-05 20:09:59 -0400

    5 April 2020
    - Updated script v4
    - Rand for all single layer model controls

--- Number_Classifier_v4/Number_Clf_v4_main.py
+++ Number_Classifier_v4/Number_Clf_v4_main.py
@@ -9,6 +9,7 @@ Number Classifier v4 - main
 
 import numpy as np
 import pandas as pd
+import os
 import time
 
 import Number_Clf_v4_utilities as utils
@@ -16,6 +17,8 @@ import Number_Clf_v4_utilities as utils
             #### MAIN EXECUTABLE ####
 
 if __name__ == '__main__':
+
+    home_path = 'C:/Users/Landon/Documents/GitHub/Convolutional-Neural-Networks/Number_Classifier_v4'
 
     MODEL_SIZES = utils.N_layer_models         # model sizes dictionary
     X_train,X_test,y_train,y_test = \
@@ -27,16 +30,50 @@ if __name__ == '__main__':
     for model_type in MODEL_SIZES.keys():               # for each MLP size
         print("Testing:",str(model_type))               # meesage to user
 
+        out_path = home_path + '/'+ str(model_type)
+        try:
+            os.mkdir(out_path)
+        except:
+            pass
+
         for layer_sizes in MODEL_SIZES[model_type]:     # for each layer size
             print("\tLayer Sizes:",layer_sizes)
 
+            output_matrix = np.array([])                # output for 100 samples 
             for I in range (0,N_iterations,1):
-                
+
+                # Create & Train Model
                 t0 = time.process_time()
-
-                model = utils.Create_MLP_Model(name=)
-
+                model = utils.Create_MLP_Model(name=test_type+'_'+str(I),
+                                               layers=layer_sizes,
+                                               X=X_train,y=y_train)
                 tf = time.process_time()
+                dt = np.round(tf-t0,decimals=0)         # time to create & train model
+                print("\t\tTime to train",model.name,":",dt)
+                
+                # Evaluate Model (scores & confmat)
+                model = utils.Eval_MLP_Model(model=model,
+                                                X=X_test,y=y_test)
+                model = utils.confusion_matrix(model,y_test,
+                                               model.predictions,show=False)
+                
+                # Add to output array
+                row = np.array([dt,model.loss_,model.n_iter_])
+                row = np.append(row,model.precision_scores)
+                row = np.append(row,model.recall_scores)
+                row = np.append(row,model.confusion_matrix)
+
+                output_matrix = np.append(output_matrix,row)    
+                del(model)
+
+            # reshape output matrix
+            output_matrix = output_matrix.reshape(N_iterations,-1)
+            output_name = test_type+'_'+str(layer_sizes[0])
+            Frame = utils.Create_DataFrame(output_matrix)
+            Frame.to_csv(out_path+'/'+output_name+'.csv')
+
+    print(time.process_time())
+                
 
 
 
--- Number_Classifier_v4/Number_Clf_v4_utilities.py
+++ Number_Classifier_v4/Number_Clf_v4_utilities.py
@@ -19,7 +19,7 @@ import sklearn.metrics as metrics
             #### VARIBALE DECLARATIONS ####
 
 N_layer_models = {
-    'single_layer_models' : [(),(20,),(40,),(60,),(80,),(100,),(120,)] ,
+    'single_layer_models' : [(20,),(40,),(60,),(80,),(100,),(120,)] ,
     'double_layer_models' : [(20,20),(40,40),(60,60),(80,80),(100,100),
                              (120,120)],
     'quadruple_layer_models' : [(20,20,20,20),(40,40,40,40),(60,60,60,60),
@@ -28,7 +28,7 @@ N_layer_models = {
             #### FUNCTION DEFINTIONS ####
 
 def train_test_data (test=0.4,seed=None):
-    """ Collect Trainign & Testing Data from sklearn.datasets """
+    """ Collect Training & Testing Data from sklearn.datasets """
     print("Collecting MNIST data .....\n")
     X,y = fetch_openml(name='mnist_784',version=1,return_X_y=True)
     X_subset,y_subset = X[:16000],y[:16000]
@@ -36,11 +36,24 @@ def train_test_data (test=0.4,seed=None):
     return train_test_split(X_subset,y_subset,
                             test_size=test,random_state=seed)
 
+def Create_DataFrame (matrix):
+    """ Create Pandas DataFrame to hold output information for each test"""
+    cols = ['Train Time','Loss Value','Iterations']
+    for metric in ['precision','recal']:        # prec & recal
+        for bin in np.arange(0,10,1):           # classes 0 - 9
+            idx = 'Class_'+str(bin)+'_'+str(metric)
+            cols.append(idx)                    # add to columns list
+    for I in range (0,100,1):                   # for 100 elements
+        cols.append('Confmat_'+str(I))          # add 100 elements of confmat
+    frame = pd.DataFrame(data=matrix,columns=cols)  # crete the frame
+    return frame                                # return the frame
+
 def Create_MLP_Model (name,layers,X,y):
     """
     Create & Train Instance of sklearn Multilayer Perceptron Class
     --------------------------------
     name (str) : name to attach to classifier instance
+    test_type (str) : Indicates the type of test on that particular CSV file
     layers (tuple) : Hidden layer sizes
     X (array) : feature training data (n_samples x n_features)
     y (array) : target training data (n_samples x 1)
@@ -48,9 +61,9 @@ def Create_MLP_Model (name,layers,X,y):
     Return trained MLP Classifier Instance
     """
     model = MLPClassifier(hidden_layer_sizes=layers,activation='relu',
-                          solver='sgd',batch_size=100,max_iters=400,
+                          solver='sgd',batch_size=100,max_iter=400,
                           tol=1e-4,random_state=None)
-    setattr(model,'name',name)          # attach name to instance
+    setattr(model,'name',name)          # name of model
     model = model.fit(X,y)              # train model
     return model                        # return trained model
 
@@ -62,13 +75,18 @@ def Eval_MLP_Model(model,X,y):
     X (array) : feature testing data (n_samples x n_features)
     y (array) : target testing data (n_samples x 1)
     --------------------------------
-    return predictions , precision & recall scores
+    return model with predictions , precision scores, & recall scores as attrbs
     """
     z = model.predict(X)                # model predictions
     labels = model.classes_             # class labels
-    prec = metrics.precision_score(y,z,labels,average=None)
-    recall = metrics.recall_score(y,z,labels,average=None)
-    return z,prec,recall                # return predictions & scores
+    setattr(model,'predictions',z)
+    setattr(model,'precision_scores',\
+        metrics.precision_score(y,z,labels,
+        average=None,zero_division=0))
+    setattr(model,'recall_scores',\
+        metrics.recall_score(y,z,labels,
+        average=None,zero_division=0))
+    return model                    # return model w/ attched attrbs
 
 def confusion_matrix (model,y,z,show=False):
     """
@@ -79,16 +97,17 @@ def confusion_matrix (model,y,z,show=False):
     z (array) : predicted testing samples (n_samples x 1)
     show (bool) : If True, visualize color-coded confusion matrix
     --------------------------------
-    Return confusion matrix (n_classes x n_classes)
+    Return model w/ confusion matrix (n_classes x n_classes) attrb
     """
     confmat = metrics.confusion_matrix(y,z,model.classes_)
+    setattr(model,'confusion_matrix',confmat)
     if show == True:
         plt.title(str(model.name),size=20,weight='bold')
         plt.xlabel('Predicted Classes',size=16,weight='bold')
         plt.ylabel('Actual Classes',size=16,weight='bold')
         plt.imshow(confmat,cmap=plt.cm.binary)
         plt.show()
-    return confmat
+    return model
 
 
 
