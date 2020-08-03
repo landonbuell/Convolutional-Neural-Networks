93fa90515ba8bcef0524962026697c9fee35a11b
landonbuell <lhb1007@wildcats.unh.edu>
2020-08-03 17:15:41 -0400

    3 August 2020 (2)
    - Updated Plots

--- Error-Compensation-Attacks/Error-Compensation-Figures/ERR_COMP_FIGS_MAIN.py
+++ Error-Compensation-Attacks/Error-Compensation-Figures/ERR_COMP_FIGS_MAIN.py
@@ -28,21 +28,21 @@ if __name__ == '__main__':
     # Plot Loss Metrics
     cols = ['Baseline Loss','Approximated Loss','33% Blurred Loss','50% Blurred Loss','Compensated Loss']
     data = [filedata[x] for x in cols]
-    utils.Plot_Metrics('4 Pixel Border Loss','Loss Function Value',data,
+    utils.Plot_Metrics('LossValue4Pixels','Loss Function Value',data,
                        'loss',True,False)
 
     # Plot Precision Metrics
     cols = ['Baseline Precision','Approximated Precision',
             '33% Blurred Precision','50% Blurred Precision','Compensated Precision']
     data = [filedata[x] for x in cols]
-    utils.Plot_Metrics('4 Pixel Border Precision Score','Precision Score Value',data,
+    utils.Plot_Metrics('PrecisionScore4Pixels','Precision Score Value',data,
                        'precision',True,False)
 
     # Plot Precision Metrics
     cols = ['Baseline Recall','Approximated Recall',
             '33% Blurred Recall ', '50% Blurred Recall ','Compensated Recall']
     data = [filedata[x] for x in cols]
-    utils.Plot_Metrics('4 Pixel Border Recall Score','Recall Score Value',data,
+    utils.Plot_Metrics('RecallScore4Pixels','Recall Score Value',data,
                        'recall',True,False)
 
             #### PLOT EXECUTION TIME PERCENTAGE ####
@@ -60,8 +60,8 @@ if __name__ == '__main__':
         diff = utils.percent_diff(baseline,np.array(filedata[col],dtype=np.float64))
         dt.append(diff)        # add the percent diff
 
-    utils.Plot_PercentDiff('Fitting Time Difference from Baseline','Percentage Difference',
-                           labs=filedata.columns,ydata=dt)
+    utils.Plot_PercentDiff('TrainingPercentDifference','Fitting-Time Difference',
+                           labs=filedata.columns,ydata=dt,save=True,show=False)
     
             #### PLOT COMEPNSATION vs. EXECUTION TIME ####
     path = 'C:\\Users\\Landon\\Documents\\GitHub\\Convolutional-Neural-Networks\\' + \
@@ -80,8 +80,8 @@ if __name__ == '__main__':
 
     plot_data = plot_data.reshape(len(infiles),-1)
     labels = ['2 Pixel Border','4 Pixel Border','6 Pixel Border','8 Pixel Border']
-    utils.Plot_PercentDiff2('Compensation in Forward Pass','Percentage of Time',
-                            labels,plot_data,show=True)
+    utils.Plot_PercentDiff2('CompensationTimePercent','Compensation-Layer Time Usage',
+                            labels,plot_data,True,False)
 
 
         
--- Error-Compensation-Attacks/Error-Compensation-Figures/ERR_COMP_FIGS_Utilities.py
+++ Error-Compensation-Attacks/Error-Compensation-Figures/ERR_COMP_FIGS_Utilities.py
@@ -14,7 +14,7 @@ import os
 
             #### DEFINITIONS ####
 
-def Plot_Metrics (title,ylab,ydata,metric,save=False,show=True):
+def Plot_Metrics (savename,ylab,ydata,metric,save=False,show=True):
     """
     Plot Metrics From a Given Dataframe
     --------------------------------
@@ -26,17 +26,17 @@ def Plot_Metrics (title,ylab,ydata,metric,save=False,show=True):
     Return None
     """
     plt.figure(figsize=(20,12))
-    plt.title(title,size=60,weight='bold',pad=20)
+    plt.title(' ',size=60,weight='bold',pad=20)
     plt.ylabel(ylab,size=40,weight='bold')
     plt.xlabel('Pixel Grouping Size',size=40,weight='bold')
 
     kernel_sides = np.array([2,3,4,5,6])
 
-    plt.plot(kernel_sides,ydata[0],color='red',linestyle='-',marker='o',ms=20,label='Baseline')
-    plt.plot(kernel_sides,ydata[1],color='blue',linestyle='-',marker='^',ms=20,label='Approximated')
-    plt.plot(kernel_sides,ydata[2],color='magenta',linestyle='-',marker='s',ms=20,label='33% Blurred')
+    plt.plot(kernel_sides,ydata[0],color='red',linestyle='-',marker='o',ms=30,label='Baseline')
+    plt.plot(kernel_sides,ydata[1],color='blue',linestyle='-',marker='^',ms=30,label='Approximated')
+    plt.plot(kernel_sides,ydata[2],color='magenta',linestyle='-',marker='s',ms=30,label='33% Blurred')
     #plt.plot(kernel_sides,ydata[3],color='green',linestyle='-',marker='s',ms=20,label='50% Blurred')
-    plt.plot(kernel_sides,ydata[4],color='gray',linestyle='-',marker='v',ms=20,label='Compensated')
+    plt.plot(kernel_sides,ydata[4],color='gray',linestyle='-',marker='v',ms=30,label='Compensated')
     
     if metric in ['precision','recall']:
         plt.yticks(np.arange(0,1.1,0.1),size=35)
@@ -44,16 +44,15 @@ def Plot_Metrics (title,ylab,ydata,metric,save=False,show=True):
         plt.yticks(size=35)
     plt.xticks(kernel_sides,['2x2','3x3','4x4','5x5','6x6'],size=40)
     plt.grid()
-    plt.legend(fontsize=25,loc=0)    
+    plt.legend(fontsize=30,loc=0)    
       
     if save == True:
-        title = title.replace(' ','_')
-        plt.savefig(title+'.png')
+        plt.savefig(savename+'.png')
     if show == True:
         plt.show()
     plt.close()
 
-def Plot_PercentDiff (title,ylab,labs,ydata,save=False,show=True):
+def Plot_PercentDiff (savename,ylab,labs,ydata,save=False,show=True):
     """
     Plot Metrics From a Given Dataframe
     --------------------------------
@@ -64,7 +63,7 @@ def Plot_PercentDiff (title,ylab,labs,ydata,save=False,show=True):
     Return None
     """
     plt.figure(figsize=(20,12))
-    plt.title(title,size=60,weight='bold',pad=20)
+    plt.title(' ',size=60,weight='bold',pad=20)
     plt.ylabel(ylab,size=40,weight='bold')
     plt.xlabel('Pixel Grouping Size',size=40,weight='bold')
 
@@ -72,29 +71,29 @@ def Plot_PercentDiff (title,ylab,labs,ydata,save=False,show=True):
     plt.hlines(0,2,7,color='black')
 
     #plt.plot(kernel_sides,ydata[0],color='blue',linestyle='-',marker='^',ms=20,label=labs[0])
-    plt.plot(kernel_sides,ydata[1],color='green',linestyle='-',marker='^',ms=20,label=labs[1])
+    plt.plot(kernel_sides,ydata[1],color='green',linestyle='-',marker='^',ms=30,label=labs[1])
     #plt.plot(kernel_sides,ydata[2],color='gray',linestyle='-',marker='^',ms=20,label=labs[2])
-    plt.plot(kernel_sides,ydata[3],color='purple',linestyle='-',marker='^',ms=20,label=labs[3])
+    plt.plot(kernel_sides,ydata[3],color='purple',linestyle='-',marker='^',ms=30,label=labs[3])
 
     #plt.plot(kernel_sides,ydata[4],color='orange',linestyle='--',marker='s',ms=20,label=labs[4])
-    plt.plot(kernel_sides,ydata[5],color='yellow',linestyle='--',marker='s',ms=20,label=labs[5])
+    plt.plot(kernel_sides,ydata[5],color='yellow',linestyle='--',marker='s',ms=30,label=labs[5])
     #plt.plot(kernel_sides,ydata[6],color='red',linestyle='--',marker='s',ms=20,label=labs[6])
-    plt.plot(kernel_sides,ydata[7],color='magenta',linestyle='--',marker='s',ms=20,label=labs[7])
+    plt.plot(kernel_sides,ydata[7],color='magenta',linestyle='--',marker='s',ms=30,label=labs[7])
 
    
-    plt.xticks(kernel_sides,['2 x 2','3 x 3','4 x 4','5 x 5','6 x 6'],size=40)
-    plt.yticks(np.arange(-15,+16,5),size=40)
+    plt.xticks(kernel_sides,['2x2','3x3','4x4','5x5','6x6'],size=40)
+    plt.yticks(np.arange(-15,+16,5),
+               ['-15%','-10%','-5%','0%','+5%','+10%','+15%'],size=40)
     plt.grid()
-    plt.legend(fontsize=25,loc=0)    
+    plt.legend(fontsize=30,loc=0)    
       
     if save == True:
-        title = title.replace(' ','_')
-        plt.savefig(title+'.png')
+        plt.savefig(savename+'.png')
     if show == True:
         plt.show()
     plt.close()
 
-def Plot_PercentDiff2 (title,ylab,labs,ydata,save=False,show=True):
+def Plot_PercentDiff2 (savename,ylab,labs,ydata,save=False,show=True):
     """
     Plot Metrics From a Given Dataframe
     --------------------------------
@@ -105,26 +104,24 @@ def Plot_PercentDiff2 (title,ylab,labs,ydata,save=False,show=True):
     Return None
     """
     plt.figure(figsize=(20,12))
-    plt.title(title,size=60,weight='bold',pad=20)
+    plt.title(' ',size=60,weight='bold',pad=20)
     plt.ylabel(ylab,size=40,weight='bold')
     plt.xlabel('Pixel Grouping Size',size=40,weight='bold')
 
     kernel_sides = np.array([2,3,4,5,6])
-    plt.hlines(0,2,7,color='black')
 
-    plt.plot(kernel_sides,ydata[0],color='blue',linestyle='-',marker='v',ms=20,label=labs[0])
-    plt.plot(kernel_sides,ydata[1],color='green',linestyle='-',marker='v',ms=20,label=labs[1])
-    plt.plot(kernel_sides,ydata[2],color='gray',linestyle='-',marker='v',ms=20,label=labs[2])
-    plt.plot(kernel_sides,ydata[3],color='purple',linestyle='-',marker='v',ms=20,label=labs[3])
+    plt.plot(kernel_sides,ydata[0],color='blue',linestyle='-',marker='v',ms=30,label=labs[0])
+    plt.plot(kernel_sides,ydata[1],color='green',linestyle='-',marker='v',ms=30,label=labs[1])
+    plt.plot(kernel_sides,ydata[2],color='gray',linestyle='-',marker='v',ms=30,label=labs[2])
+    plt.plot(kernel_sides,ydata[3],color='purple',linestyle='-',marker='v',ms=30,label=labs[3])
 
     plt.xticks(kernel_sides,['2 x 2','3 x 3','4 x 4','5 x 5','6 x 6'],size=40)
-    plt.yticks(np.arange(-0,+11,2),size=40)
+    plt.yticks(np.arange(4,13,2),['4%','6%','8%','10%','12%'],size=40)
     plt.grid()
-    plt.legend(fontsize=25,loc=4)    
+    plt.legend(fontsize=30,loc=0)    
       
     if save == True:
-        title = title.replace(' ','_')
-        plt.savefig(title+'.png')
+        plt.savefig(savename+'.png')
     if show == True:
         plt.show()
     plt.close()
