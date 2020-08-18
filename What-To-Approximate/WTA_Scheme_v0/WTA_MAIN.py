e3455bccc3a25adfee37089187866f9093e8d93f
landonbuell <lhb1007@wildcats.unh.edu>
2020-08-18 18:17:33 -0400

    18 August 2020
    - Ran Baseline for WTA scheme
    Need to re-run for more epochs

---
+++ What-To-Approximate/WTA_Scheme_v0/WTA_MAIN.py
@@ -0,0 +1,59 @@
+"""
+Landon Buell
+Qioayan Yu
+What-to-Approximate Scheme
+18 August 2020
+"""
+
+        #### IMPORTS ####
+
+import numpy as np
+import pandas as pd
+import os
+import time
+import tensorflow.keras as keras
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import WTA_Utilities as utils
+
+        #### MAIN EXECUTABLE ####
+
+if __name__ == '__main__':
+
+    homePath = os.getcwd()
+    outputPath = os.path.join(os.path.dirname(homePath),'Raw_Data')
+    outputBuffer = os.path.join(outputPath,utils.FrameName)
+
+    outputFrame = pd.DataFrame(data=None,columns=utils.FrameCols)
+    outputFrame.to_csv(path_or_buf=outputBuffer,mode='w')
+
+    N_iters = 2
+   
+    X_train,y_train,X_test,y_test = utils.CIFAR10()
+    y_train = keras.utils.to_categorical(y_train,10)
+    y_test = keras.utils.to_categorical(y_test,10)
+
+    for SIZE in utils.KERNELSIZES:
+        print("\tTesting Kernel Size:",SIZE)
+        likeModelData = np.array([])        # hold data for similar models
+
+        for i in range (N_iters):
+            NETWORK = utils.NeuralNetwork('SINGLE_'+str(SIZE),SIZE)
+            NETWORK.__TRAIN__(X_train,y_train)
+            scores = NETWORK.__EVALUATE__(X_test,y_test)
+
+            likeModelData = np.append(likeModelData,scores)
+            
+
+        likeModelData = likeModelData.reshape(N_iters,-1)      
+        averageScores = np.mean(likeModelData,axis=0)
+        exportData = np.array([NETWORK.modelName])
+        exportData = np.append(exportData,averageScores)
+    
+        frame = pd.DataFrame(data=[exportData],columns=utils.FrameCols)
+        frame.to_csv(path_or_buf=outputBuffer,mode='a',header=False)
+
+
+    print("=)")
+
---
+++ What-To-Approximate/WTA_Scheme_v0/WTA_Utilities.py
@@ -0,0 +1,92 @@
+"""
+Landon Buell
+Qioayan Yu
+What-to-Approximate Scheme
+18 August 2020
+"""
+
+        #### IMPORTS ####
+
+import numpy as np
+import pandas as pd
+import os
+import tensorflow as tf
+import tensorflow.keras as keras
+
+        #### VARIABLE DECLARATIONS ####
+
+KERNELSIZES = np.array([2,3,4,5,6])
+
+FrameCols = ['Model Name','Loss','Precision','Recall']
+FrameName = 'Baseline.csv'
+
+        #### FUNCTIONS DEFINITIONS ####
+
+def CIFAR10 ():
+    """ Collect CiFar 10 Data Set from tf.keras """
+    print("Collecting CiFar-10 Dataset:")
+    (X_train,y_train),(X_test,y_test) = keras.datasets.cifar10.load_data()
+    return X_train,y_train,X_test,y_test
+
+
+
+        #### CLASS DEFINTIONS ####
+
+class WhatToApproximateLayer (keras.layers.Layer):
+   """ What-To-Approximate Layer For CNN Network """
+   
+   def __init__(self):
+       """ Initialize Class Object Instance """
+       super(WhatToApproximateLayer,self).__init__(trainable=False)
+
+
+class NeuralNetwork :
+    """ Create Nerual Network Models """
+
+    def __init__(self,name,kernelSizes):
+        """ Initialize Class Intance """
+        self.modelName = name
+        self.kernelSizes = ([kernelSizes,kernelSizes],)
+        self.model = self.ConvolutionalNeuralNetwork()
+
+    def ConvolutionalNeuralNetwork (self):
+        """
+        Create Single Convolutional Neural Network
+        --------------------------------
+        name (str) : Name to Use for model instance
+        kernelSizes (iter) : Iterable of ints determining wall sides of CNN kernel
+        --------------------------------
+        Return Compiled Keras CNN
+        """
+        model = keras.models.Sequential(name=self.modelName)
+        model.add(keras.layers.InputLayer(input_shape=(32,32,3),name='Input'))
+
+        # Each Layer Group
+        for i,side in enumerate(self.kernelSizes):
+            model.add(keras.layers.Conv2D(filters=64,kernel_size=side,strides=(1,1),padding='same',
+                                            activation='relu',name='C'+str(i+1)+'A'))
+            model.add(keras.layers.Conv2D(filters=64,kernel_size=side,strides=(1,1),padding='same',
+                                            activation='relu',name='C'+str(i+1)+'B'))
+            model.add(keras.layers.MaxPool2D(pool_size=(2,2),name='P'+str(i+1)))
+
+        # Dense Layers
+        model.add(keras.layers.Flatten(name='F1'))
+        model.add(keras.layers.Dense(units=128,activation='relu',name='D1'))
+        model.add(keras.layers.Dense(units=10,activation='softmax',name='Output'))
+ 
+        # Complie & Return
+        model.compile(optimizer=keras.optimizers.Adam(),
+                      loss=keras.losses.CategoricalCrossentropy(),
+                      metrics=['Precision','Recall'])
+        #print(model.summary())
+        return model
+
+    def __TRAIN__(self,X,Y):
+        """ Train Neural Network Model """     
+        self.model.fit(x=X,y=Y,batch_size=128,epochs=16,verbose=2)
+        return self
+
+    def __EVALUATE__(self,X,Y):
+        """ Evaluate Neural Network Model """
+        scores = self.model.evaluate(x=X,y=Y,verbose=0)
+        return scores
